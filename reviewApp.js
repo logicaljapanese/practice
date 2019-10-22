ac85af49ab1683dacd7aa6f9d85919aedf3179ea
Jacob Hedges <jacob.hedges@epicgames.com>
2019-10-22 01:25:28 -0400

    adding vocab sets 1 to 6

--- reviewApp.js
+++ reviewApp.js
@@ -8,6 +8,21 @@ var gbKatakana = false;
 const KATAKANADIACRITICSANDDIGRAPHS = "katakanaDiacriticsAndDigraphs";
 var gbKatakanaDD = false;
 
+const VOCAB1 = "vocab1";
+var gbVocab1 = false;
+const VOCAB2 = "vocab2";
+var gbVocab2 = false;
+const VOCAB3 = "vocab3";
+var gbVocab3 = false;
+const VOCAB4 = "vocab4";
+var gbVocab4 = false;
+const VOCAB5 = "vocab5";
+var gbVocab5 = false;
+const VOCAB6 = "vocab6";
+var gbVocab6 = false;
+const VOCAB7 = "vocab7";
+var gbVocab7 = false;
+
 // This is what we're currently reviewing
 var gReviewCharacters = {};
 
@@ -31,12 +46,33 @@ function changeReviewState(content, state)
         case KATAKANADIACRITICSANDDIGRAPHS.toLowerCase():
             gbKatakanaDD = state;
             break;
+        case VOCAB1.toLowerCase():
+            gbVocab1 = state;
+            break;
+        case VOCAB2.toLowerCase():
+            gbVocab2 = state;
+            break;
+        case VOCAB3.toLowerCase():
+            gbVocab3 = state;
+            break;
+        case VOCAB4.toLowerCase():
+            gbVocab4 = state;
+            break;
+        case VOCAB5.toLowerCase():
+            gbVocab5 = state;
+            break;
+        case VOCAB6.toLowerCase():
+            gbVocab6 = state;
+            break;
+        case VOCAB7.toLowerCase():
+            gbVocab7 = state;
+            break;
         default:
             return false;
     }
     
     // User has nothing selected, tell them to select something
-    if (!(gbHiragana || gbHiraganaDD || gbKatakana || gbKatakanaDD))
+    if (!(gbHiragana || gbHiraganaDD || gbKatakana || gbKatakanaDD || gbVocab1 || gbVocab2 || gbVocab3 || gbVocab4 || gbVocab5 || gbVocab6 || gbVocab7))
     {
         resetQuestionElement();
         return false;
@@ -49,12 +85,24 @@ function changeReviewState(content, state)
     let hiraganaDDCharacters = gbHiraganaDD ? getHirganaDDCharacters() : {};
     let katakanaCharacters = gbKatakana ? getKatakanaCharacters() : {};
     let katakanaDDCharacters = gbKatakanaDD ? getKatakanaDDCharacters() : {};
+    let vocab1Characters = gbVocab1 ? getVocabSet1() : {};
+    let vocab2Characters = gbVocab2 ? getVocabSet2() : {};
+    let vocab3Characters = gbVocab3 ? getVocabSet3() : {};
+    let vocab4Characters = gbVocab4 ? getVocabSet4() : {};
+    let vocab5Characters = gbVocab5 ? getVocabSet5() : {};
+    let vocab6Characters = gbVocab6 ? getVocabSet6() : {};
 
     gReviewCharacters = {
         ...hiraganaCharacters,
         ...hiraganaDDCharacters,
         ...katakanaCharacters,
-        ...katakanaDDCharacters
+        ...katakanaDDCharacters,
+        ...vocab1Characters,
+        ...vocab2Characters,
+        ...vocab3Characters,
+        ...vocab4Characters,
+        ...vocab5Characters,
+        ...vocab6Characters
     };
 
     getNextCharacter(document.getElementById("currentQuestion"));
@@ -221,4 +269,60 @@ function getKatakanaDDCharacters()
             "ジャ":["jya"],"ジュ":["jyu"],"ジョ":["jyo"],
             "ビャ":["bya"],"ビュ":["byu"],"ビョ":["byo"],
             "ピャ":["pya"],"ピュ":["pyu"],"ピョ":["pyo"]}
+}
+
+function getVocabSet1()
+{
+    return {"おちゃ":["tea"],"りんご":["apple"],"パン":["bread"],"あめ":["hard candy", "rain"],"みず":["water"],
+            "犬":["dog"],"ねこ":["cat"],"とり":["bird"],"ぶた":["pig"],"さかな":["fish"],
+            "くるま":["car"],"ひこうき":["airplane"],"じてんしゃ":["bicycle"],"バス":["bus"],"タクシー":["taxi"],
+            "いす":["chair"],"テーブル":["table"],"つくえ":["desk"],"テレビ":["tv", "television"],"ベッド":["bed"]}
+}
+
+function getVocabSet2()
+{
+    return {"上手":["good at","skillful"],"下手":["bad at","unskillful"],"かんたん":["easy"],"だめ":["no good"],
+            "すてき":["splendid","lovely","amazing"],"いろいろ":["various"],"たくさん":["a lot", "many"],"きけん":["dangerous"],
+            "きれい":["pretty","clean"],"ていねい":["polite"],"しつれい":["rude"],"ゆうめい":["famous"],"まじめ":["dilligent","serious"],"りっぱ":["splendid","fine"],
+            "たいせつ":["important"],"ひつよう":["necessary"],"べんり":["convenient"],"ふべん":["inconvenient"],"とくべつ":["special"],"たいへん":["serious","very","grave","great"]}
+}
+
+function getVocabSet3()
+{
+    return {"大きい":["big"],"小さい":["small"],"おそい":["late","slow"],"はやい":["early","fast"],"おおい":["many"],
+            "すくない":["few"],"あたらしい":["new"],"ふるい":["old"],"ひろい":["wide"],"せまい":["narrow"],
+            "よい":["good"],"わるい":["bad"],"かるい":["light"],"おもい":["heavy"],"高い":["high","tall","expensive"],
+            "安い":["cheap","inexpensive"],"つよい":["strong"],"よわい":["weak"],"あつい":["hot","thick"],"さむい":["cold"]}
+}
+
+function getVocabSet4()
+{
+    return {"ペン":["pen"],"えんぴつ":["pencil"],"かみ":["paper","god","hair"],"せんせい":["teacher"],"きょうしつ":["classroom"],
+            "きょうかしょ":["textbook"],"本":["book"],"としょかん":["library"],"せんぱい":["one's senior"],"こうはい":["one's junior"],
+            "おとうさん":["father"],"おかあさん":["mother"],"おにいさん":["older brother"],"おとうとさん":["younger brother"],"おねえさん":["older sister"],
+            "いもうとさん":["younger sister"],"おや":["parent"],"子ども":["children"],"きょうだい":["siblings","brothers"],"かぞく":["family"]}
+}
+
+function getVocabSet5()
+{
+    return {"日":["sun"],"月":["moon"],"ほし":["star"],"木":["tree"],"くさ":["grass"],
+            "山":["mountain"],"川":["river"],"海":["sea"],"かいがん":["beach"],"こうえん":["park"],
+            "いえ":["house"],"マンション":["apartment"],"だいどころ":["kitchen"],"へや":["room"],"おてあらい":["bathroom"],
+            "わたし":["i","me"],"あなた":["you"],"かれ":["he","him"],"かのじょ":["she","her"],"もの":["thing","physical thing"]}
+}
+
+function getVocabSet6()
+{
+    return {"言う":["to say"],"話す":["to talk"],"いく":["to go"],"くる":["to come"],"でかける":["to go out"],
+            "かえる":["to return home","to go home","to change"],"食べる":["to eat"],"飲む":["to drink"],"みる":["to see","to watch"],"みせる":["to show"],
+            "あそぶ":["to play"],"あるく":["to walk"],"はしる":["to run"],"およぐ":["to swim"],"うたう":["to sing"],
+            "きく":["to listen","to hear","to ask"],"みえる":["to be seen","to be visible","to be able to see"],"のぼる":["to climb"],"つかれる":["to get tired"],"やすむ":["to rest"]}
+}
+
+function getVocabSet7()
+{
+    return {"":[""],"":[""],"":[""],"":[""],"":[""],
+            "":[""],"":[""],"":[""],"":[""],"":[""],
+            "":[""],"":[""],"":[""],"":[""],"":[""],
+            "":[""],"":[""],"":[""],"":[""],"":[""]}
 }
